662cc285a6e31e4378eb1789641cf26cd38f4c3d
Nicholas Wieland <ngw@nofeed.org>
2021-07-08 14:50:01 +0100

    Rework db connection

--- lib/blueshift.rb
+++ lib/blueshift.rb
@@ -1,13 +1,25 @@
 require 'blueshift/railtie' if defined?(Rails)
 require 'blueshift/version'
-require 'blueshift/migration'
+require 'logger'
 require 'sequel'
 require 'sequel/adapters/redshift'
 require 'sequel/extensions/schema_dumper_ext'
 require 'sequel/extensions/redshift_schema_dumper'
 
 module Blueshift
-  def self.migration(&block)
-    Blueshift::Migration.new(&block)
+  def self.redshift
+    @redshift ||= Sequel.connect(ENV.fetch('REDSHIFT_URL', 'redshift://'), logger: Logger.new('redshift.log'))
+  end
+
+  def self.postgres
+    @postgres ||= Sequel.connect(ENV.fetch('DATABASE_URL', 'postgres://'), logger: Logger.new('postgres.log'))
+  end
+
+  def self.connect
+    if ENV['REDSHIFT_URL'].exists?
+      redshift
+    elsif ENV['DATABASE_URL'].exists?
+      postgres
+    end
   end
 end
