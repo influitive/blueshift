7e18023024622429d5881c89a8d05b916d251128
Nicholas Wieland <ngw@nofeed.org>
2021-07-14 12:58:43 +0100

    Remove conditional connection

--- lib/blueshift.rb
+++ lib/blueshift.rb
@@ -7,19 +7,4 @@ require 'sequel/extensions/schema_dumper_ext'
 require 'sequel/extensions/redshift_schema_dumper'
 
 module Blueshift
-  def self.redshift
-    @redshift ||= Sequel.connect(ENV.fetch('REDSHIFT_URL', 'redshift://'), logger: Logger.new('redshift.log'))
-  end
-
-  def self.postgres
-    @postgres ||= Sequel.connect(ENV.fetch('DATABASE_URL', 'postgres://'), logger: Logger.new('postgres.log'))
-  end
-
-  def self.connect
-    if ENV['REDSHIFT_URL'].exists?
-      redshift
-    elsif ENV['DATABASE_URL'].exists?
-      postgres
-    end
-  end
 end
